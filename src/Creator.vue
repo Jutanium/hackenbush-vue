83f5e3fb1a99d0da748cd44375afad918c9e0bda
Dan Jutan <danjutan@gmail.com>
2021-07-05 12:45:49 -0400

    player size

--- src/Creator.vue
+++ src/Creator.vue
@@ -1,33 +1,32 @@
 <template>
-  <button @click="this.create = !this.create">{{this.create ? "Play" : "Create"}}</button>
-  <div>
+  <div class="root">
+    <button @click="this.create = !this.create">{{ this.create ? "Play" : "Create" }}</button>
     <component :is="currentComponent" v-bind="currentComponentProps"></component>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 import GameCreator from "./components/creator/GameCreator.vue";
 import GamePlayer from "@/components/player/GamePlayer.vue";
 import {state} from "@/components/creator/state/game-file";
-
 export default defineComponent({
   components: {
     GameCreator
   },
-  data () {
+  data() {
     return {
       create: true
     }
   },
   computed: {
-    currentComponent () {
+    currentComponent() {
       if (this.create) {
         return GameCreator;
       }
       return GamePlayer;
     },
-    currentComponentProps () {
+    currentComponentProps() {
       if (!this.create) {
         return {
           segments: state.segments,
@@ -40,4 +39,7 @@ export default defineComponent({
 </script>
 
 <style>
+div.root {
+  width: 600px;
+}
 </style>
