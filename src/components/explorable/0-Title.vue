e1bc320d2720d50883851435eccb8e75aec455f4
Dan Jutan <danjutan@gmail.com>
2021-08-25 11:38:34 -0400

    title screen spacing

--- src/components/explorable/0-Title.vue
+++ src/components/explorable/0-Title.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="h-screen w-full flex flex-col items-center">
-    <div class="h-1/3"></div>
-    <div class="ml-6 md:ml-0 text-5xl font-bold mt-20" ref="title">
+    <div class="ml-6 md:ml-0 text-5xl font-bold mt-44" ref="title">
       The Most Mathematical Game
     </div>
     <div class="text-2xl font-bold mt-8" ref="subtitle">
@@ -16,7 +15,7 @@
     <div ref="scroll" class="text-center mt-16 mb-4">
       Scroll to Continue
     </div>
-    <div class="relative lg:-top-36 w-1/2 -mt-36" ref="demo">
+    <div class="transform -translate-y-1/2 w-64 sm:w-80 md:w-96 lg:w-1/2 max-w-xl" ref="demo">
       <GameCreator :demoMode="true"/>
     </div>
   </div>
@@ -49,9 +48,9 @@ onMounted(() => {
   fadeIn.fromTo(title.value, {opacity: 0}, {opacity: 1, duration: 1.5})
   fadeIn.fromTo([demo.value], {opacity: 0}, {opacity: 1, duration: 2}, "-=1.5")
   fadeIn.fromTo([subtitle.value], {opacity: 0}, {opacity: 1, duration: 2}, "-=0.8")
-  fadeIn.fromTo(byline.value, {opacity: 0}, {opacity: 1, duration: 1.5}, "-=0.8")
-  fadeIn.fromTo(artcredit.value, {opacity: 0}, {opacity: 1, duration: 2}, "-=0.3")
-  fadeIn.fromTo(scroll.value, {opacity: 0}, {opacity: 1, duration: 1, repeat: -1, yoyo: true}, "-=0.5")
+  fadeIn.fromTo(byline.value, {opacity: 0}, {opacity: 1, duration: 1.2}, "-=0.8")
+  fadeIn.fromTo(artcredit.value, {opacity: 0}, {opacity: 1, duration: 1.2})
+  fadeIn.fromTo(scroll.value, {opacity: 0}, {opacity: 1, duration: 1, repeat: -1, yoyo: true}, "-=0.1")
 })
 </script>
 
